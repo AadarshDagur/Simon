889411c5c0fdf3c6b6b72e35658e70d2d0b5b651
Adarsh Chaudhary <124592867+AadarshDagur@users.noreply.github.com>
2024-05-30 23:22:37 +0530

    Update game.js

--- game.js
+++ game.js
@@ -63,9 +63,10 @@ $(".container .btn").click(function (){
         userClickedPattern=[];
         var audio1=new Audio("./sounds/wrong.mp3");
         audio1.play();
-        $("h2").text("GAME OVER..PRESS ANY KEY TO RESTART THE GAME")
+        $("h2").text("GAME OVER..PRESS ANY KEY OR CLICK BELOW TO RESTART THE GAME")
         $("body").addClass("game-over");
         setTimeout(function (){$("body").removeClass("game-over");},200);
+        $(".start").slideDown(70);
     }  
     i++;
 
